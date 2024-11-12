2cc936492fea2fcc41fdde5beab559c49a3d1d96
takahasidaiki <167832834+takahasidaiki@users.noreply.github.com>
2024-11-12 10:05:17 +0900

    Update ex06-8.js

--- ex06-8.js
+++ ex06-8.js
@@ -7,3 +7,4 @@ function hanako68(){document.getElementById('who68').innerHTML = '花子';}*/
 function sayhello68(){alert('Hello,' + document.getElementById('who68').innerHTML + '!');}
 function taro68(){document.getElementById('who68').innerHTML = '太郎';}
 function hanako68(){document.getElementById('who68').innerHTML = '花子';}
+function gazo688(){document.getElementById('who688').innerHTML='<img src="./image/Rapunzels_Lantern_Festival.jpg" alt="オン">';}
