6eeaed3158411dae8578433f10ab4c9c93805add
takahasidaiki <167832834+takahasidaiki@users.noreply.github.com>
2024-11-15 02:09:27 +0900

    Update ex06-10.js

--- ex06-10.js
+++ ex06-10.js
@@ -1,1 +1,2 @@
 function imgchange1(){document.getElementById('logo').src='./image/Rapunzels_Lantern_Festival.jpg';}
+function resetImage(){document.getElementById('logo').src='./image/png_transparent_josai_university_hd_logo.png';}
