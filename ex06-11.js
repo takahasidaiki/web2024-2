514454d175375f33feb4b0a03c001a229974a12c
takahasidaiki <167832834+takahasidaiki@users.noreply.github.com>
2024-11-26 09:37:32 +0900

    Update ex06-11.js

--- ex06-11.js
+++ ex06-11.js
@@ -1,1 +1,2 @@
 function imgchange2(){document.getElementById('logo2').src='./image/channels4_profile.jpg';}
+function resetImage2(){document.getElementById('logo').src='./image/3c8a56c5-c833-4525-ac91-9bc7a2b7b357.jpg';}
