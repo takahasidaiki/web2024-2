8628ce89c8761f925838d89738c21f2bab2de263
takahasidaiki <167832834+takahasidaiki@users.noreply.github.com>
2024-12-03 10:35:52 +0900

    Update ex06-14.js

--- ex06-14.js
+++ ex06-14.js
@@ -1,1 +1,3 @@
-function imgchange3(){document.getElementById('logo3').src='./image/Anna_and_Elsas_Frozen_Journey.jpg';}
+function imgchange3(){document.getElementById('logo3').src='./image/Anna_and_Elsas_Frozen_Journey.jpg';
+                     setTimeout(resetImage3,3000);//3秒後に元の画像に戻す}
+function resetImage3(){document.getElementById('logo3').src='./image/jokun.jpg';}
