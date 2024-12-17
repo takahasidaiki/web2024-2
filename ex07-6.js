c87c56ad83a6db9463ae65ea41d78e187c38521b
takahasidaiki <167832834+takahasidaiki@users.noreply.github.com>
2024-12-17 10:32:45 +0900

    Update ex07-6.js

--- ex07-6.js
+++ ex07-6.js
@@ -1,1 +1,11 @@
-function nazo76(){if(document.getElementById('answer76').value < '夢'){alert('はずれ')}else{alert('あたり！')};}
+//function nazo76(){if(document.getElementById('answer76').value < '夢'){alert('はずれ')}else{alert('あたり！')};}
+function nazo76() {
+    var answer = document.getElementById('answer76').value;
+    if (answer < '夢') {
+        alert('はずれ');
+    } else if (answer === '夢') {
+        alert('あたり！');
+    } else {
+        alert('はずれ');
+    }
+}
