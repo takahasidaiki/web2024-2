28dfc73aaf5bca48dd5d92f754b030c0d785a6c2
takahasidaiki <167832834+takahasidaiki@users.noreply.github.com>
2024-12-17 09:49:02 +0900

    Update ex07-5.js

--- ex07-5.js
+++ ex07-5.js
@@ -1,1 +1,1 @@
-function nazo75(){if(document.getElementById('answer75').value == '夢'){alert('あたり！');};}
+function nazo75(){if(document.getElementById('answer75').value == '夢'){alert('あたり！')}else{alert('はずれ')};}
