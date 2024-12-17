31eaf2184397a4820dcc39937fff394668b1600e
takahasidaiki <167832834+takahasidaiki@users.noreply.github.com>
2024-12-17 10:08:44 +0900

    Update ex07-6.js

--- ex07-6.js
+++ ex07-6.js
@@ -1,3 +1,2 @@
-//function nazo(){if(document.getElementById('answer').value == '夢'){alert('あたり！')};}
-//function nazo75(){if(document.getElementById('answer75').value == '夢'){alert('あたり！')}else{alert('はずれ')};}
+
 function nazo76(){if(document.getElementById('answer76').value < '夢'){alert('はずれ')}else{alert('あたり！')};}
