47921351c89f12a1d4c950d041bae4ba8b3f97dc
takahasidaiki <167832834+takahasidaiki@users.noreply.github.com>
2024-12-24 10:42:13 +0900

    Update ex07-10.js

--- ex07-10.js
+++ ex07-10.js
@@ -1,1 +1,4 @@
-
+function BMI2(){var x = Number(document.getElementById('bmi3').value);
+              var y = Number(document.getElementById('bmi4').value);
+               var bmi2 = x / (y * y);
+              alert(bmi2);}
