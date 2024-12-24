06062691ce60065d75f0b858516d62dfa568d852
takahasidaiki <167832834+takahasidaiki@users.noreply.github.com>
2024-12-24 10:19:36 +0900

    Create ex07-9.js

---
+++ ex07-9.js
@@ -0,0 +1,9 @@
+function nazo79(){
+  if(document.getElementById('answer79').value == '夢'){
+  alert('あたり！');
+}else if(document.getElementById('answer79').value == 'ゆめ'){
+    alert('あたり！');
+  }else if(){
+    alert('はずれ！');
+  }
+                 }
