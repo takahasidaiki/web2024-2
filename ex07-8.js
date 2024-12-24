0961089e4186e5a4e1485734018911f8e82ac519
takahasidaiki <167832834+takahasidaiki@users.noreply.github.com>
2024-12-24 10:03:56 +0900

    Update ex07-8.js

--- ex07-8.js
+++ ex07-8.js
@@ -1,5 +1,5 @@
-function nazo77(){
-  if(document.getElementById('answer77').value == '夢'){
+function nazo78(){
+  if(document.getElementById('answer78').value == '夢'){
   alert('あたり！');
 }else{
   alert('はずれ！');
