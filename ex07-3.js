4ef7e889be3f39978d32d64bd8adba02ddd6a481
takahasidaiki <167832834+takahasidaiki@users.noreply.github.com>
2025-01-31 21:32:39 +0900

    Update ex07-3.js

--- ex07-3.js
+++ ex07-3.js
@@ -1,4 +1,4 @@
 function BMI(){var x = Number(document.getElementById('bmi1').value);
               var y = Number(document.getElementById('bmi2').value);
-               var bmi = x / (y * y);
+               var bmi = (x * 10000) / (y * y);
               alert(bmi);}
