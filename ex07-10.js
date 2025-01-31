20bd71a46da827772ace2bf02b18cd6704bf7744
takahasidaiki <167832834+takahasidaiki@users.noreply.github.com>
2025-01-31 21:49:05 +0900

    Update ex07-10.js

--- ex07-10.js
+++ ex07-10.js
@@ -3,11 +3,11 @@ function BMI2(){var x = Number(document.getElementById('bmi3').value);
                var bmi2 = (x * 10000) / (y * y);
                 var result;
 
-                if (bmi2 < 18.5) {
+                /*if (bmi2 < 18.5) {
         result = "低体重";
     } else if (bmi2 >= 18.5 && bmi2 < 25) {
         result = "普通";
     } else {
         result = "肥満";
     }
-              alert("BMI: " + bmi2 + " = " + result);}
+              alert("BMI: " + bmi2 + " = " + result);}*/
